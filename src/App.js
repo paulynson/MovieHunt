63b09d33d6770b56e44c25bb70b3dbac182e02d7
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2022-05-01 22:34:27 +0100

    Fixed pages loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './styles.css';
 import Header from './component/Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './component/Home';
 
 function App() {
   return (
     <div className="font-serif">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
        <Header />
-        <Routes>
-         <Route exact path="/" element={ <Home />} />
-          </Routes>
-        </BrowserRouter>
+       <Home />
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,12 @@
 import heroimg from "../assets/image/heroimg.jpg";
-import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div>
       <section className="containers h-44 md:h-44 lg:h-44 flex items-center bg-black py-10 px-20">
-        <Link to="/" className="text-white border-white border-2 px-5 py-3.5 text-xl">
+        <a href="/" className="text-white border-white border-2 px-5 py-3.5 text-xl">
         MovieHunt
-        </Link>
+        </a>
         
       </section>
       <nav className="heroContainer flex relative items-center">
